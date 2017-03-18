e286d3a7dd3f0d38c9d88fcafed20c3f7012320a
mergar <olegginzburg@gmail.com>
2017-03-18 03:25:06 +0300

    wip

--- modules/public/mysql/lib/puppet/type/mysql_user.rb
+++ modules/public/mysql/lib/puppet/type/mysql_user.rb
@@ -29,8 +29,10 @@ Puppet::Type.newtype(:mysql_user) do
 
       mysql_version = Facter.value(:mysql_version)
       unless mysql_version.nil?
-        if Puppet::Util::Package.versioncmp(mysql_version, '10.0.0') < 0 and user_part.size > 16
+        if Puppet::Util::Package.versioncmp(mysql_version, '5.7.8') < 0 and user_part.size > 16
           raise(ArgumentError, 'MySQL usernames are limited to a maximum of 16 characters')
+        elsif Puppet::Util::Package.versioncmp(mysql_version, '10.0.0') < 0 and user_part.size > 32
+          raise(ArgumentError, 'MySQL usernames are limited to a maximum of 32 characters')
         elsif Puppet::Util::Package.versioncmp(mysql_version, '10.0.0') > 0 and user_part.size > 80
           raise(ArgumentError, 'MySQL usernames are limited to a maximum of 80 characters')
         end
@@ -71,6 +73,33 @@ Puppet::Type.newtype(:mysql_user) do
   newproperty(:max_updates_per_hour) do
     desc "Max updates per hour for the user. 0 means no (or global) limit."
     newvalue(/\d+/)
+  end
+
+  newproperty(:tls_options, :array_matching => :all) do
+    desc "Options to that set the TLS-related REQUIRE attributes for the user."
+    validate do |value|
+      value = [value] if not value.is_a?(Array)
+      if value.include? 'NONE' or value.include? 'SSL' or value.include? 'X509'
+        if value.length > 1
+          raise(ArgumentError, "REQUIRE tls options NONE, SSL and X509 cannot be used with other options, you may only use one of them.")
+        end
+      else
+        value.each do |opt|
+          if not o = opt.match(/^(CIPHER|ISSUER|SUBJECT)/i)
+            raise(ArgumentError, "Invalid tls option #{o}")
+          end
+        end
+      end
+    end
+    def insync?(is)
+      # The current value may be nil and we don't
+      # want to call sort on it so make sure we have arrays
+      if is.is_a?(Array) and @should.is_a?(Array)
+        is.sort == @should.sort
+      else
+        is == @should
+      end
+    end
   end
 
 end
