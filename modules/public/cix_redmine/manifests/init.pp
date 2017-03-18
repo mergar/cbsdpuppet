6ac88d017c056d2d692eaf0ea0f5bda2db160934
mergar <olegginzburg@gmail.com>
2017-03-18 20:22:57 +0300

    wip

--- modules/public/cix_redmine/manifests/init.pp
+++ modules/public/cix_redmine/manifests/init.pp
@@ -13,4 +13,16 @@ class cix_redmine(
 #		require => File["$workdir/cbsd.conf"],
 	}
 
+	file { "/usr/local/etc/rc.d/redmine":
+		mode => '0555',
+		ensure  => present,
+		content => template("${module_name}/redmine.erb"),
+	}
+
+#	file { "/usr/local/etc/rc.d/redmine":
+#		mode => '0555',
+#		ensure  => present,
+#		source => [ "puppet://${server}/modules/${module_name}/openssh-lpk-openldap.schema" ],
+#	}
+
 }
